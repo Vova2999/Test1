0a8cd4937e046f9ea8a6f4f6678a18463dfe5f85
Vova2999 <Vova2999@gmail.com>
2024-04-04 20:25:28 +0400

    Sort by asc

--- Test1/Source.cpp
+++ Test1/Source.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <Windows.h>
 using namespace std;
@@ -19,6 +20,7 @@ void main()
 		system("cls");
 
 		cout << "1. Вывести массив" << endl;
+		cout << "2. Отсортировать по возрастанию" << endl;
 		cout << "0. Выход" << endl;
 
 		int input;
@@ -34,6 +36,10 @@ void main()
 				cout << arr[i] << " ";
 			cout << endl;
 			system("pause");
+			break;
+		case 2:
+			sort(arr, arr + size);
+			break;
 		}
 	}
 
